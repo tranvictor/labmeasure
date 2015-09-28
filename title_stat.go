a53ac72a81f52e535a5811c4208f1eb138a28617
Victor Tran <vu.tran54@gmail.com>
2015-09-28 11:12:06 +0700

    add title measurement

--- title_stat.go
+++ title_stat.go
@@ -11,3 +11,10 @@ type TitleStat struct {
 func (ts TitleStat) GetIncorrectRecords() interface{} {
 	return ts.IncorrectRecords
 }
+
+func (ts TitleStat) Accuracy() float32 {
+	if ts.Examined == 0 {
+		return 0.0
+	}
+	return float32(ts.Correct) / float32(ts.Examined)
+}
