86bdd207906eb7ce633f6fd1de70d94175ca7d67
Victor Tran <vu.tran54@gmail.com>
2015-09-29 10:12:03 +0700

    limit concurrency level

--- measure.go
+++ measure.go
@@ -36,31 +36,55 @@ func initFinalStat(length int, comparers []Comparer) FinalStat {
 	return result
 }
 
-func analyze(darticles, larticles Articles, conf Config, comparers ...Comparer) FinalStat {
-	result := initFinalStat(len(larticles), comparers)
-	sem := make(chan empty, len(larticles))
+type Batch struct {
+	LArticle Article
+	Index    int
+}
+
+func splitArticles(larticles Articles, num int) []map[string]Batch {
+	result := []map[string]Batch{}
+	for i := 0; i < num; i++ {
+		result = append(result, map[string]Batch{})
+	}
 	index := 0
 	for url, larticle := range larticles {
+		result[index%num][url] = Batch{
+			larticle,
+			index,
+		}
+		index += 1
+	}
+	return result
+}
+
+func analyze(darticles, larticles Articles, conf Config, comparers ...Comparer) FinalStat {
+	result := initFinalStat(len(larticles), comparers)
+	concurrency := 100
+	sem := make(chan empty, concurrency)
+	concurrencyBatches := splitArticles(larticles, concurrency)
+	for _, batch := range concurrencyBatches {
 		go func(
-			index int, url string, result *FinalStat,
-			larticle Article, darticles *Articles) {
-			fmt.Printf("%d\n", index)
-			if darticle, exist := (*darticles)[url]; exist {
-				for _, comparer := range comparers {
-					record := comparer.Compare(
-						darticle, larticle, conf)
-					if record != nil {
-						record.SetURL(url)
-						result.AddRecordFor(
-							comparer.Name(), index, record)
+			result *FinalStat,
+			batches map[string]Batch, darticles *Articles) {
+			for url, batch := range batches {
+				larticle := batch.LArticle
+				index := batch.Index
+				if darticle, exist := (*darticles)[url]; exist {
+					for _, comparer := range comparers {
+						record := comparer.Compare(
+							darticle, larticle, conf)
+						if record != nil {
+							record.SetURL(url)
+							result.AddRecordFor(
+								comparer.Name(), index, record)
+						}
 					}
 				}
 			}
 			sem <- empty{}
-		}(index, url, &result, larticle, &darticles)
-		index += 1
+		}(&result, batch, &darticles)
 	}
-	for i := 0; i < len(larticles); i++ {
+	for i := 0; i < concurrency; i++ {
 		<-sem
 	}
 	for _, comparer := range comparers {
